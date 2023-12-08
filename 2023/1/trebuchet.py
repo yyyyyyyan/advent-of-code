9b9be2bde1beb14a2aaf3e0e0aed420f28e3e0d0
yyyyyyyan <contact@yyyyyyyan.tech>
2023-12-07 21:05:43 -0300

    Complete day 1.

---
+++ 2023/1/trebuchet.py
@@ -0,0 +1,28 @@
+def trebuchet(puzzle_input):
+    input_lines = puzzle_input.strip().split("\n")
+    result = 0
+    for line in input_lines:
+        line_digits = [char for char in line if char.isdigit()]
+        if len(line_digits) >= 1:
+            calibration = int(line_digits[0] + line_digits[-1])
+            result += calibration
+    return result
+
+
+def trebuchet_final(puzzle_input):
+    spelled_out_numbers = [
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ]
+    for number, spelled_out_number in enumerate(spelled_out_numbers, start=1):
+        puzzle_input = puzzle_input.replace(
+            spelled_out_number, spelled_out_number + str(number) + spelled_out_number
+        )
+    return trebuchet(puzzle_input)
